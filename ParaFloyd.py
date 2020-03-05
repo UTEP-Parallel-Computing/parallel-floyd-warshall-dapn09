326bffffd15f5e13763a6d2e7c7561bc851f9105
dapn09 <dapn09@gmail.com>
2020-03-05 16:31:24 -0700

    code finished, displaying inputs and outputs

--- ParaFloyd.py
+++ ParaFloyd.py
@@ -3,6 +3,7 @@
 #Description: This script implements the Floyd-Warshall algorithm in parallel using MPI.
 
 from mpi4py import MPI
+import matrixUtils as mu
 
 def readMatrixFromFile(filePath):
     file = open(filePath, 'r')
@@ -12,21 +13,36 @@ def readMatrixFromFile(filePath):
     return lines
 
 if __name__ == "__main__":
-    refGraph = readMatrixFromFile("fwTestResult.txt")
-    testGraph = readMatrixFromFile("fwTest.txt")
-
-    size = len(testGraph)
-    #Make matrix which will store the results.
-    solvedGraph = [[0 for _ in range(size)] for __ in range(size)]
 
     #get world communicator
     comm = MPI.COMM_WORLD
 
+    #Read the file containing the test matrix.
+    testGraph = readMatrixFromFile("fwTest.txt")
+    #Read the file with the correct answers.
+    refGraph = readMatrixFromFile("fwTestResult.txt")
+
+    if comm.Get_rank() == 0:
+
+        print("\nInput matrices:\n\n")
+        print("Test Matrix:")
+        mu.printSubarray(testGraph, size=6)
+        #New line
+        print()
+
+        print("\nTarget Matrix:")
+        mu.printSubarray(refGraph, size=6)
+        #New line
+        print()
+
+    #Useful formulas
+    size = len(testGraph)
     rowsPerThread = size//comm.Get_size()
     threadsPerRow = comm.Get_size()/size
     startRow = rowsPerThread*comm.Get_rank()
     endRow = rowsPerThread*(comm.Get_rank() + 1)
 
+    #Parallel Floyd-Warshall algorithm
     for k in range(size):
         ownerOfRow = int(threadsPerRow*k)
         testGraph[k] = comm.bcast(testGraph[k], root=int(threadsPerRow*k))
@@ -35,21 +51,32 @@ if __name__ == "__main__":
                 if i != j:
                     testGraph[i][j] = min(int(testGraph[i][j]), int(testGraph[i][k]) + int(testGraph[k][j]))
 
-
+    #Assemble results
     if comm.Get_rank() == 0:
         for k in range(endRow, size):
             ownerOfK = int((comm.Get_size()/size)*k)
             testGraph[k] = comm.recv(source=ownerOfK, tag=42)
 
-        #compare results
+        print("Output matrices:\n\n")
+        print("Test Matrix:")
+        mu.printSubarray(testGraph, size=6)
+        print()
+        print("\nTarget Matrix:")
+        mu.printSubarray(refGraph, size=6)
+        print()
+
+        #Compare results
+        discrepFound = False
         for i in range(size):
             for j in range(size):
                 sol = int(refGraph[i][j])
                 tst = int(testGraph[i][j])
                 if sol != tst:
-                    print(f"Discrepancies found in row:{i}, col:{j}!!!")
-                else:
-                    print("Both matrices match!!!")
+                    discrepFound = True
+        if discrepFound:
+            print(f"Discrepancies found!!!")
+        else:
+            print("Both matrices match!!!")
     else:
         for k in range(startRow, endRow):
             comm.send(testGraph[k], dest=0, tag=42)
